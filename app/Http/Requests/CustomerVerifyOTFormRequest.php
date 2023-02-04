24c73dda5a743ccb131185d51424a4bd35e2fd0f
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-04 11:18:08 +0700

    api

--- app/Http/Requests/CustomerVerifyOTFormRequest.php
+++ app/Http/Requests/CustomerVerifyOTFormRequest.php
@@ -4,10 +4,11 @@ namespace App\Http\Requests;
 
 use App\Traits\PhoneNumberSerializable;
 use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\FailedValidation;
 
 class CustomerVerifyOTFormRequest extends FormRequest
 {
-    use PhoneNumberSerializable;
+    use PhoneNumberSerializable, FailedValidation;
 
     /**
      * Determine if the user is authorized to make this request.
@@ -34,4 +35,12 @@ class CustomerVerifyOTFormRequest extends FormRequest
         ];
     }
 
+    public function messages()
+    {
+        return [
+            'idd.required' => 'idd_is_required',
+            'otp.required' => 'otp_is_required',
+            'phone_number.required' => 'phone_number_is_required',
+        ];
+    }
 }
