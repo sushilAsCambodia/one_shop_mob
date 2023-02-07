b271ede2a3fa4a9297526a2542a0d930da6ca1b5
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-07 10:02:30 +0700

    order products

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -17,7 +17,7 @@ class OrderProduct extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class)->with['image','deal'];
+        return $this->belongsTo(Product::class)->with(['image','deal']);
     }
 
 
