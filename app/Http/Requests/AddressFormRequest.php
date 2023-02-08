db4986e27036a3c5d78702e2fa73d8d0e0e086d1
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-08 16:10:07 +0700

    address add api

--- app/Http/Requests/AddressFormRequest.php
+++ app/Http/Requests/AddressFormRequest.php
@@ -26,12 +26,27 @@ class AddressFormRequest extends FormRequest
     {
 
         return [
-            'street_address_1' => $this->method() === 'POST' ? 'required': "",
-            'street_address_2' => $this->method() === 'POST' ? 'nullable': "",
-            'country_id' => $this->method() === 'POST' ? 'required|exists:countries,id': 'exists:countries,id',
-            'state_id' => $this->method() === 'POST' ? 'required|exists:states,id': "exists:states,id",
-            'city_id' => $this->method() === 'POST' ? 'required|exists:cities,id': "exists:cities,id",
-            'pincode' => $this->method() === 'POST' ? 'required': "",
+            'street_address_1' => $this->method() === 'POST' ? 'required' : "",
+            'street_address_2' => $this->method() === 'POST' ? 'nullable' : "",
+            'country_id' => $this->method() === 'POST' ? 'required|exists:countries,id' : 'exists:countries,id',
+            'state_id' => $this->method() === 'POST' ? 'required|exists:states,id' : "exists:states,id",
+            'city_id' => $this->method() === 'POST' ? 'required|exists:cities,id' : "exists:cities,id",
+            'pincode' => $this->method() === 'POST' ? 'required' : "",
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'street_address_1.required' => 'address_line_1_is_required',
+            'street_address_2.required' => 'address_line_2_is_required',
+            'country_id.required' => 'country_id_is_required',
+            'country_id.exists' => 'country_id_is_not_exists',
+            'state_id.required' => 'state_id_is_required',
+            'state_id.exists' => 'state_id_is_not_exists',
+            'city_id.required' => 'city_id_is_required',
+            'city_id.exists' => 'city_id_is_not_exists',
+            'pincode.required' => 'pincode_is_required',
         ];
     }
 }
