bc105cde1a55e436c59bdcc7a7ac3ac1d4971aa6
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-09 09:38:11 +0700

    order api

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -29,7 +29,7 @@ class Order extends Model
 
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'order_product');
+        return $this->belongsToMany(Product::class, 'order_product')->with(['image']);
     }
 
     public function shipping()
