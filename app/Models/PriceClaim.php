17c4044cbcff249d2b12403c359e07de22b57fd5
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-09 18:05:25 +0700

    claim

--- app/Models/PriceClaim.php
+++ app/Models/PriceClaim.php
@@ -16,7 +16,7 @@ class PriceClaim extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class)->with(['image', 'translation']);
     }
 
 
