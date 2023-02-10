2fc73c1f9630cb5891877019c20a30db032724fa
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-10 11:42:56 +0700

    claim

--- app/Services/PriceClaimService.php
+++ app/Services/PriceClaimService.php
@@ -2,16 +2,18 @@
 
 namespace App\Services;
 
-use App\Models\Address;
 use App\Models\PriceClaim;
 use App\Models\Shipping;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class PriceClaimService
 {
+
+    public function __construct(private AddressService $addressService)
+    {
+    }
 
     public function paginate($request): JsonResponse
     {
@@ -47,7 +49,7 @@ class PriceClaimService
                     });
             });
 
-            $results = $query->select('price_claims.*')->with(['product' , 'product.slotDeals', 'customer', 'slot_deals', 'order'])->paginate($perPage, ['*'], 'page', $page);
+            $results = $query->select('price_claims.*')->with(['product', 'product.slotDeals', 'customer', 'slot_deals', 'order'])->paginate($perPage, ['*'], 'page', $page);
 
             return response()->json($results, 200);
         } catch (\Exception $e) {
@@ -59,82 +61,65 @@ class PriceClaimService
     public function prizeClaimByBookingId($request): JsonResponse
     {
         // try {
-            $query = (new PriceClaim())->newQuery();
+        $query = (new PriceClaim())->newQuery();
 
-            // $query->when($request->dates, function ($query) use ($request) {
-            //     if ($request->dates[0] == $request->dates[1]) {
-            //         $query->whereDate('price_claims.created_at', Carbon::parse($request->dates[0])->format('Y-m-d'));
-            //     } else {
-            //         $query->whereBetween('price_claims.created_at', [
-            //             Carbon::parse($request->dates[0])->startOfDay(),
-            //             Carbon::parse($request->dates[1])->endOfDay(),
-            //         ]);
-            //     }
-            // });
+        // $query->when($request->dates, function ($query) use ($request) {
+        //     if ($request->dates[0] == $request->dates[1]) {
+        //         $query->whereDate('price_claims.created_at', Carbon::parse($request->dates[0])->format('Y-m-d'));
+        //     } else {
+        //         $query->whereBetween('price_claims.created_at', [
+        //             Carbon::parse($request->dates[0])->startOfDay(),
+        //             Carbon::parse($request->dates[1])->endOfDay(),
+        //         ]);
+        //     }
+        // });
 
-            $query->where('status', '!=', 'completed');
+        $query->where('status', '!=', 'completed');
 
-            // $query->where('customer_id', auth()->user()->id);
+        // $query->where('customer_id', auth()->user()->id);
 
-            // $query->when($request->order_booking_id, function ($query) use ($request) {
-            //     $query->leftJoin('slot_deals', 'slot_deals.id', 'price_claims.booking_id')
-            //         ->leftJoin('orders', 'orders.id', 'price_claims.order_id')
-            //         ->where(function ($q) use ($request) {
-            //             $q->where('orders.order_id', 'like', "%$request->order_booking_id%")
-            //                 ->orWhere('slot_deals.booking_id', 'like', "%$request->order_booking_id%");
-            //         });
-            // });
+        // $query->when($request->order_booking_id, function ($query) use ($request) {
+        //     $query->leftJoin('slot_deals', 'slot_deals.id', 'price_claims.booking_id')
+        //         ->leftJoin('orders', 'orders.id', 'price_claims.order_id')
+        //         ->where(function ($q) use ($request) {
+        //             $q->where('orders.order_id', 'like', "%$request->order_booking_id%")
+        //                 ->orWhere('slot_deals.booking_id', 'like', "%$request->order_booking_id%");
+        //         });
+        // });
 
-            $results['price_claims'] = $query->select('price_claims.*')->where('booking_id', '=', $request->bookingId)
-                            ->with(['product' , 'order'])->first();
+        $results['price_claims'] = $query->select('price_claims.*')->where('booking_id', '=', $request->bookingId)
+            ->with(['product', 'order'])->first();
 
-            // foreach ($results as $key1 => $result) {
-            //     // dd($result);
-            //     $orderProductData = OrderProduct::with('product.deal', 'product.slotDeals')->where('order_id', $result->id)->where('status', $slug)
-            //         ->with(['product'])->latest('created_at')->get();
+        // foreach ($results as $key1 => $result) {
+        //     // dd($result);
+        //     $orderProductData = OrderProduct::with('product.deal', 'product.slotDeals')->where('order_id', $result->id)->where('status', $slug)
+        //         ->with(['product'])->latest('created_at')->get();
 
-            //     foreach ($orderProductData as $key => $orderProduct) {
-            //         $deal = Deal::whereProductId($orderProduct->product_id)->where('deals.status', 'active')->orderBy('created_at', 'desc')->first();
-            //         if (!empty($deal) && $deal) {
-            //             $slotsId = $deal->slots()->first()->id;
-            //             $orderProductData[$key]->slots_deals = SlotDeal::where('order_id', $result->id)
-            //                 ->where('slot_id', $slotsId)->where('is_bot', 0)->count();
-            //         }
-            //     }
+        //     foreach ($orderProductData as $key => $orderProduct) {
+        //         $deal = Deal::whereProductId($orderProduct->product_id)->where('deals.status', 'active')->orderBy('created_at', 'desc')->first();
+        //         if (!empty($deal) && $deal) {
+        //             $slotsId = $deal->slots()->first()->id;
+        //             $orderProductData[$key]->slots_deals = SlotDeal::where('order_id', $result->id)
+        //                 ->where('slot_id', $slotsId)->where('is_bot', 0)->count();
+        //         }
+        //     }
 
-            //     $results[$key1]->order_product = $orderProductData;
-            // }
-            
-            $query2 =  (new Address())->newQuery();
+        //     $results[$key1]->order_product = $orderProductData;
+        // }
 
-            $modelData = Auth::user();
+        $results['addresses'] = $this->addressService->all($request);
 
-            $query2->when($modelData, function ($query2) use ($modelData) {
-                $query2->whereAddressableType(Customer::class)
-                    ->whereAddressableId($modelData->id);
-            });
-            $results['addresses'] = $query2->select(
-                'addresses.id',
-                'addresses.street_address_1',
-                'addresses.street_address_2',
-                'addresses.pincode',
-                'addresses.country_id',
-                'addresses.state_id',
-                'addresses.city_id'
-            )->get();
-
-
-            $result['message'] = 'fetch_price_claim_successfully';
-            $result['data'] = $results;
-            $result['statusCode'] = 200;
-            return getSuccessMessages($result);
+        $result['message'] = 'fetch_price_claim_successfully';
+        $result['data'] = $results;
+        $result['statusCode'] = 200;
+        return getSuccessMessages($result);
 
         // } catch (\Exception $e) {
         //     \Log::debug($e);
         //     return generalErrorResponse($e);
         // }
     }
-    
+
     public function update($priceClaim, array $data): JsonResponse
     {
         try {
@@ -153,5 +138,4 @@ class PriceClaimService
             return generalErrorResponse($e);
         }
     }
-
 }
