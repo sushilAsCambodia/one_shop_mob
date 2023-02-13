00671bf2bc6606b226564e4b4a315e0ec75092a4
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-13 15:32:15 +0700

    to ship

--- app/Services/PriceClaimService.php
+++ app/Services/PriceClaimService.php
@@ -17,7 +17,7 @@ class PriceClaimService
 
     public function paginate($request): JsonResponse
     {
-        // try {
+        try {
             $perPage = $request->rowsPerPage ?: 15;
             $page = $request->page ?: 1;
             $sortBy = $request->sortBy ?: 'created_at';
@@ -25,40 +25,40 @@ class PriceClaimService
 
             $query = (new PriceClaim())->newQuery()->orderBy($sortBy, $sortOrder);
 
-            // $query->when($request->dates, function ($query) use ($request) {
-            //     if ($request->dates[0] == $request->dates[1]) {
-            //         $query->whereDate('price_claims.created_at', Carbon::parse($request->dates[0])->format('Y-m-d'));
-            //     } else {
-            //         $query->whereBetween('price_claims.created_at', [
-            //             Carbon::parse($request->dates[0])->startOfDay(),
-            //             Carbon::parse($request->dates[1])->endOfDay(),
-            //         ]);
-            //     }
-            // });
+            $query->when($request->dates, function ($query) use ($request) {
+                if ($request->dates[0] == $request->dates[1]) {
+                    $query->whereDate('price_claims.created_at', Carbon::parse($request->dates[0])->format('Y-m-d'));
+                } else {
+                    $query->whereBetween('price_claims.created_at', [
+                        Carbon::parse($request->dates[0])->startOfDay(),
+                        Carbon::parse($request->dates[1])->endOfDay(),
+                    ]);
+                }
+            });
 
-            $query->where('status', '!=', 'completed');
+            $query->where('status', '!=', 'completed')->where('status', '!=', 'shipping');
 
             $query->where('customer_id', auth()->user()->id);
 
-            // $query->when($request->order_booking_id, function ($query) use ($request) {
-            //     $query->leftJoin('slot_deals', 'slot_deals.id', 'price_claims.booking_id')
-            //         ->leftJoin('orders', 'orders.id', 'price_claims.order_id')
-            //         ->where(function ($q) use ($request) {
-            //             $q->where('orders.order_id', 'like', "%$request->order_booking_id%")
-            //                 ->orWhere('slot_deals.booking_id', 'like', "%$request->order_booking_id%");
-            //         });
-            // });
+            $query->when($request->order_booking_id, function ($query) use ($request) {
+                $query->leftJoin('slot_deals', 'slot_deals.id', 'price_claims.booking_id')
+                    ->leftJoin('orders', 'orders.id', 'price_claims.order_id')
+                    ->where(function ($q) use ($request) {
+                        $q->where('orders.order_id', 'like', "%$request->order_booking_id%")
+                            ->orWhere('slot_deals.booking_id', 'like', "%$request->order_booking_id%");
+                    });
+            });
 
-            $results = $query->select('price_claims.*')
-                        ->with(['product'])
-                        // ->with(['product', 'slot_deals', 'order'])
-                        ->paginate($perPage, ['*'], 'page', $page);
+            $results = $query->select('price_claims.*')->with(['product' , 'product.slotDeals', 'customer', 'slot_deals.deal:id,deal_id', 'order'])->paginate($perPage, ['*'], 'page', $page);
 
-            return response()->json($results, 200);
-        // } catch (\Exception $e) {
-        //     \Log::debug($e);
-        //     return generalErrorResponse($e);
-        // }
+            $result['message'] = 'fetch_price_claim_successfully';
+            $result['data'] = $results;
+            $result['statusCode'] = 200;
+            return getSuccessMessages($result);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+            return generalErrorResponse($e);
+        }
     }
 
     public function prizeClaimByBookingId($request): JsonResponse
