ba5fc3f7ef66263f6d6399efa828936b5c2fa54e
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-17 15:11:16 +0700

    update on deals

--- app/Services/DealService.php
+++ app/Services/DealService.php
@@ -14,8 +14,31 @@ class DealService
     public function getSlotDeals($deal, $orderId)
     {
         try {
-            $slotDeals = SlotDeal::where('order_id', $orderId)->where('deal_id', $deal)
-                        ->select('slot_deals.id','slot_deals.booking_id','slot_deals.status')->get();
+            // $slotDeals = SlotDeal::where('order_id', $orderId)->where('deal_id', $deal)
+            //             ->select('slot_deals.id','slot_deals.booking_id','slot_deals.status')->get();
+
+            // $result['message'] = 'slotDeals_fetch_successfully';
+            // $result['data'] = $slotDeals;
+            // $result['statusCode'] = 200;
+            // return getSuccessMessages($result);
+
+
+            $orderProducts = OrderProduct::where('deal_id', $deal->id)->whereIn('status', ['confirmed', 'winner', 'loser'])
+                ->select('slot_deals.id', 'slot_deals.booking_id', 'slot_deals.status')
+                ->where('customer_id', auth()->user()->id)->get();
+                
+            $orderIds = array();
+            if ($orderId == null) {
+                $orderIds = collect($orderProducts->pluck('order_id'));
+            } else {
+                array_push($orderIds, $orderId);
+            }
+
+            if (!$orderIds && empty($orderIds)) {
+                return response()->json(['messages' => ['Data Not Found'],], 400);
+            }
+
+            $slotDeals = SlotDeal::whereIn('order_id', $orderIds)->whereIn('status', ['confirmed', 'winner', 'loser'])->where('deal_id', $deal->id)->get();
 
             $result['message'] = 'slotDeals_fetch_successfully';
             $result['data'] = $slotDeals;
