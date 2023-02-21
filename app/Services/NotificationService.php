c01a3878a04146cf104bcc7e8275ac5a9e986bb9
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-21 16:43:06 +0700

    update payment api

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -41,7 +41,7 @@ class NotificationService
                         $datas = $item->data->data;
                         $datas->read_at = $item->read_at;
                         $datas->id = $item->id;
-                        $datas->header = $item;
+                        $datas->header = $datas->slug;
                         $datas->is_read = $item->read_at ? true : false;
                         $datas->message = $item->data->message;
                         $datas->date = Carbon::parse($item->created_at)->format('Y-m-d H:m:s');
