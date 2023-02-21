234bcd5daae72f585ef0c5c789c360b27a57f876
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-21 16:40:11 +0700

    update payment api

--- app/helpers.php
+++ app/helpers.php
@@ -10,12 +10,11 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Http;
 
 const TRANSFER_IN  = 'transfer_in';
 const TRANSFER_OUT  = 'transfer_out';
 
-if (!function_exists('getErrorMessages')) {
+if (! function_exists('getErrorMessages')) {
     function getErrorMessages($messages)
     {
         $errorMessages = [];
@@ -32,15 +31,14 @@ if (!function_exists('getErrorMessages')) {
 
 // $bookingId = "SD-".Carbon::now()->timestamp;
 
-if (!function_exists('uploadImage')) {
-    function uploadImage($file, $folder)
-    {
+if(! function_exists('uploadImage')){
+    function uploadImage($file, $folder){
 
-        if (!empty($file) && is_file($file)) {
+        if (! empty($file) && is_file($file)) {
             $md5Name = md5_file($file->getRealPath());
-            $md5Name = Carbon::now()->timestamp . $md5Name;
+            $md5Name = Carbon::now()->timestamp.$md5Name;
             $guessExtension = $file->guessExtension();
-            $uploaded_files = $file->storeAs('public/images/' . $folder, $md5Name . '.' . $guessExtension);
+            $uploaded_files = $file->storeAs('public/images/'.$folder, $md5Name.'.'.$guessExtension);
             $uploaded_files = substr(Storage::url($uploaded_files), 1);
 
             return $uploaded_files;
@@ -64,7 +62,7 @@ if (!function_exists('getArrayCollections')) {
     }
 }
 
-if (!function_exists('generalErrorResponse')) {
+if (! function_exists('generalErrorResponse')) {
     function generalErrorResponse(Exception $e)
     {
         \Log::debug($e);
@@ -75,7 +73,7 @@ if (!function_exists('generalErrorResponse')) {
     }
 }
 
-if (!function_exists('paginate')) {
+if (! function_exists('paginate')) {
     function paginate($items, $perPage = 100, $page = null, $options = [])
     {
         $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
@@ -85,11 +83,11 @@ if (!function_exists('paginate')) {
     }
 }
 
-if (!function_exists('checkFileType')) {
+if (! function_exists('checkFileType')) {
     function checkFileType($file)
     {
         $type = 'file';
-        if (substr($file->getMimeType(), 0, 5) == 'image') {
+        if(substr($file->getMimeType(), 0, 5) == 'image') {
             $type = 'image';
         }
 
@@ -97,10 +95,10 @@ if (!function_exists('checkFileType')) {
     }
 }
 
-if (!function_exists('getRandomIdGenerate')) {
+if (! function_exists('getRandomIdGenerate')) {
     function getRandomIdGenerate($prefix = null)
     {
-        return $prefix . '-' . Carbon::now()->timestamp . mt_rand(100, 99999);
+        return $prefix.'-'.Carbon::now()->timestamp.mt_rand(100, 99999);
     }
 }
 if (!function_exists('generateReferralCode')) {
@@ -124,14 +122,14 @@ if (!function_exists('generateReferralCode')) {
     }
 }
 
-if (!function_exists('getSuccessMessages')) {
+if (! function_exists('getSuccessMessages')) {
     function getSuccessMessages($data, $status = true)
     {
         $successMessage = [];
-        if (!empty($data['message'])) {
+        if (! empty($data['message'])) {
             $successMessage['message'] = $data['message'];
         }
-        if (!empty($data['data'])) {
+        if (! empty($data['data'])) {
             $successMessage['data'] = $data['data'];
         }
         $successMessage['status'] = $status;
@@ -140,11 +138,11 @@ if (!function_exists('getSuccessMessages')) {
     }
 }
 
-if (!function_exists('getCustomerData')) {
+if (! function_exists('getCustomerData')) {
     function getCustomerData(Customer $customer, $level = null)
     {
         $datas = array();
-        if ($customer) {
+        if($customer){
             $mlmConfiguration = json_decode(Configure::where('type', 'MLM')->select('data')->first()->data);
 
             $L1Customers = Customer::withSum('order', 'amount')->where('parent_referral_code', $customer->referral_code)->get();
@@ -156,14 +154,14 @@ if (!function_exists('getCustomerData')) {
             $L3Customers = Customer::withSum('order', 'amount')->whereIn('parent_referral_code', $L2CustomersId)->get();
             $L3CustomersId = collect($L3Customers->pluck('referral_code'));
 
-            $L1OrdersValue = OrderProduct::whereIn('customer_id', $L1CustomersId)->where(function ($q) {
-                $q->where('status', 'confirmed')->orWhere('status', 'winner');
+            $L1OrdersValue = OrderProduct::whereIn('customer_id', $L1CustomersId)->where(function($q) {
+                $q->where('status','confirmed')->orWhere('status','winner');
             })->sum('amount');
-            $L2OrdersValue = OrderProduct::whereIn('customer_id', $L2CustomersId)->where(function ($q) {
-                $q->where('status', 'confirmed')->orWhere('status', 'winner');
+            $L2OrdersValue = OrderProduct::whereIn('customer_id', $L2CustomersId)->where(function($q) {
+                $q->where('status','confirmed')->orWhere('status','winner');
             })->sum('amount');
-            $L3OrdersValue = OrderProduct::whereIn('customer_id', $L3CustomersId)->where(function ($q) {
-                $q->where('status', 'confirmed')->orWhere('status', 'winner');
+            $L3OrdersValue = OrderProduct::whereIn('customer_id', $L3CustomersId)->where(function($q) {
+                $q->where('status','confirmed')->orWhere('status','winner');
             })->sum('amount');
 
             $obj1 = new stdClass();
@@ -174,7 +172,7 @@ if (!function_exists('getCustomerData')) {
             $obj1->members_count = count($L1Customers);
             $obj1->transaction_amount = $L1OrdersValue;
 
-            if ($L1OrdersValue > 0 && $mlmConfiguration->level_one_status == 'active')
+            if($L1OrdersValue > 0 && $mlmConfiguration->level_one_status == 'active')
                 $obj1->commission = ($mlmConfiguration->commission * $L1OrdersValue) /  100;
             else
                 $obj1->commission = 0;
@@ -183,7 +181,7 @@ if (!function_exists('getCustomerData')) {
             $obj2->members_count = count($L2Customers);
             $obj2->transaction_amount = $L1OrdersValue;
 
-            if ($L2OrdersValue > 0 && $mlmConfiguration->level_two_status == 'active')
+            if($L2OrdersValue > 0 && $mlmConfiguration->level_two_status == 'active')
                 $obj2->commission = ($mlmConfiguration->level_two_commission  *  $L2OrdersValue) / 100;
             else
                 $obj2->commission = 0;
@@ -192,7 +190,7 @@ if (!function_exists('getCustomerData')) {
             $obj3->members = $L3Customers;
             $obj3->members_count = count($L3Customers);
             $obj3->transaction_amount = $L3OrdersValue;
-            if ($L3OrdersValue > 0 && $mlmConfiguration->level_three_status == 'active')
+            if($L3OrdersValue > 0 && $mlmConfiguration->level_three_status == 'active')
                 $obj3->commission = ($mlmConfiguration->level_three_commission * $L3OrdersValue) /  100;
             else
                 $obj3->commission = 0;
@@ -203,7 +201,7 @@ if (!function_exists('getCustomerData')) {
             $datas['level_two'] = $obj2;
             $datas['level_three'] = $obj3;
             $datas['total_members'] = count($L1Customers) + count($L2Customers) + count($L3Customers);
-            $datas['total_members_data'] = getArrayCollections([$L1Customers, $L2Customers, $L3Customers]);
+            $datas['total_members_data'] = getArrayCollections([$L1Customers,$L2Customers, $L3Customers]);
             $datas['total_transaction'] = $obj1->transaction_amount + $obj2->transaction_amount + $obj3->transaction_amount;
             $datas['total_commissions'] = $obj1->commission + $obj2->commission + $obj3->commission;
         }
@@ -216,27 +214,7 @@ if (!function_exists('sendOTP')) {
     {
 
         $otpValue = rand(100000, 999999);
-        // $otpValue = 123456;
-        $header         = 'KK Exchange';
-        $textMessage    = ' Your OTP is: ' . $otpValue . ' New OTP Send Message';
-        $urlName = "https://api.mekongsms.com/api/sendsms.aspx?username=" . env('SMS_USERNAME') . "&pass=" . env('SMS_PASSWORD') . "&cd=&sender=" . $header . "&smstext=" . $textMessage . "&gsm=" . $idd . $phoneNumber . "&int=1";
-
-        $response = Http::get($urlName);
-        // return $response;
-        $contains = str_contains($response, 'Success');
-
-        if ($contains === false) {
-
-            $result['message'] = 'The_message_failed_with_status_False';
-            $result['statusCode'] = 400;
-
-            return getSuccessMessages($result, false);
-        }
-
-        // $result['message'] = 'OTP Send';
-        // $result['statusCode'] = 201;
-
-        // return getSuccessMessages($result);
+        $otpValue = 123456;
         //save otp in table
         $otp = OntimePassword::whereIdd($idd)->wherePhoneNumber($phoneNumber)->whereType($type)->whereValue($otpValue)->first();
         if ($otp)
@@ -267,9 +245,9 @@ if (!function_exists('verifyOTP')) {
             return ['status' => false, 'msg' => 'incorrect_otp'];
     }
 }
+            
 
-
-if (!function_exists('zeroappend')) {
+if (! function_exists('zeroappend')) {
     function zeroappend($LastNumber)
     {
         $count = (int) log10(abs($LastNumber)) + 1;
