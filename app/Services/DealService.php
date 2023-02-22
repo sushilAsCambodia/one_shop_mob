acc8714a5114241788e80ca0cdfbb767e0938fcb
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-22 12:32:23 +0700

    claim

--- app/Services/DealService.php
+++ app/Services/DealService.php
@@ -81,7 +81,7 @@ class DealService
             $slotDeals = SlotDeal::whereIn('order_id', $orderIds)->where('deal_id', $deal->id)->get();
 
             $result['message'] = 'slotDeals_fetch_successfully';
-            $result['data'] = $$orderProducts;
+            $result['data'] = $orderProducts;
             $result['statusCode'] = 200;
             return getSuccessMessages($result);
         } catch (\Exception $e) {
