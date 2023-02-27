3126153652d4c5285f66bb9dc59feb4ec1c6c39b
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-02-27 17:08:31 +0700

    all slots

--- app/Services/DealService.php
+++ app/Services/DealService.php
@@ -78,7 +78,7 @@ class DealService
             $slotDeals = SlotDeal::whereIn('order_id', $orderIds)->where('deal_id', $deal->id)->get();
 
             $result['message'] = 'slotDeals_fetch_successfully';
-            $result['data'] = count($slotDeals);
+            $result['data'] = $slotDeals;
             $result['statusCode'] = 200;
             return getSuccessMessages($result);
         } catch (\Exception $e) {
