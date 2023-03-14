8b1deb6f21525d7aacd21faabe3847f0bcbc8ef6
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-03-14 09:43:10 +0700

    mlm api

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -432,7 +432,7 @@ class CustomerService
     public function getCalculations($request, $customer): JsonResponse
     {
         try {
-            if(!Configure::where('type', 'MLM')->select('data')->first()){
+            if (!Configure::where('type', 'MLM')->select('data')->first()) {
                 $result['message'] = 'Please_set_the_Configuration';
                 $result['statusCode'] = 400;
                 return getSuccessMessages($result, false);
@@ -600,40 +600,40 @@ class CustomerService
         return !empty($result) ? $result : [];
     }
 
-    
+
     public function deleteOwnAccount($data): JsonResponse
     {
         try {
             $customer = Customer::find(auth()->id());
 
-            if(!$customer){
+            if (!$customer) {
                 $results['message'] = 'customer_not_found';
                 $results['statusCode'] = 201;
                 return getSuccessMessages($results, false);
             }
-            
-                // return response()->json(['message' => 'Customer not found'], 400);
+
+            // return response()->json(['message' => 'Customer not found'], 400);
 
             //check if the customer has pending orders
-            $remainingOrder = OrderProduct::whereCustomerId(auth('customer')->id())->where('status','!=','confirmed')->get();
-            if(sizeof($remainingOrder)){
+            $remainingOrder = OrderProduct::whereCustomerId(auth('customer')->id())->where('status', '!=', 'confirmed')->get();
+            if (sizeof($remainingOrder)) {
                 $results['message'] = 'customer_still_remaining_pending_order';
                 $results['statusCode'] = 201;
                 return getSuccessMessages($results, false);
             }
-                // return response()->json(['message' => 'Customer still remaining pending order'], 400);
+            // return response()->json(['message' => 'Customer still remaining pending order'], 400);
 
             //check if the customer has pending orders
-            $remainingPendingWithdraw = withDrawAmount(['Review','Approve','Pending']);
-            if ( $remainingPendingWithdraw > 0){
+            $remainingPendingWithdraw = withDrawAmount(['Review', 'Approve', 'Pending']);
+            if ($remainingPendingWithdraw > 0) {
                 $results['message'] = 'customer_still_remaining_pending_withdraw_request';
                 $results['statusCode'] = 201;
                 return getSuccessMessages($results, false);
             }
-                // return response()->json(['message' => 'Customer still remaining pending withdraw request'], 400);
+            // return response()->json(['message' => 'Customer still remaining pending withdraw request'], 400);
 
             $customer->delete();
-            
+
             $results['message'] = 'deleted_own_account_successfully';
             $results['statusCode'] = 200;
             return getSuccessMessages($results);
