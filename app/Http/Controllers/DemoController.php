47d7e28d9797126077c626258d71aa2a4bcb02d4
Sushil Gupta <sushil.as.cambodia@gmail.com>
2023-03-30 17:36:43 +0700

    broadcost setting

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -163,7 +163,7 @@ class DemoController extends Controller
 
         $broadcast = Broadcast::where('status', 'active')->get();
         $result['message'] = 'Orders_fetch_successfully';
-            $result['data'] = $broadcast['en'];
+            $result['data'] = $broadcast->en;
             $result['statusCode'] = 200;
 
         return getSuccessMessages($result);
